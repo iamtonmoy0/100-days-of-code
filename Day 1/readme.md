f13db95e74f285798f401819e55261c4448fcaff
iamtonmoy0 <tonmoy2code@gmail.com>
2023-05-27 02:21:11 -0400

    day 1

---
+++ Day 1/readme.md
@@ -0,0 +1,11 @@
+## Day - 1
+
+# Day 1 contains ~>
+
+* print on CLI
+* Concatenation of data 
+* String manipulation
+* Input data from CLI
+* Check type of data
+* Variable Declare
+
---
+++ Day 1/typeCheck.py
@@ -0,0 +1,10 @@
+#this will return length of text
+print(len('hello'))
+
+# this will type of the quoted data
+
+print(type('hello'))
+print(type(2))
+print(type(2.2))
+print(type(True))
+
